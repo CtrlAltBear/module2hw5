219f8ebd5c77136c9346da2e190c3cfc3d537778
CtrlAltBear <ctrl_alt_bear@protonmail.com>
2021-07-19 22:03:39 +0300

    no message

---
+++ Modul2HW5/Modul2HW5/Actions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Modul2HW5
+{
+    public class Actions
+    {
+        private readonly ILogger _logger;
+
+        public Actions(ILogger log)
+        {
+            _logger = log;
+        }
+
+        public bool FirstMethod()
+        {
+            _logger.WriteInfo($"Старт {nameof(FirstMethod)}:");
+            return true;
+        }
+
+        public void SecondMethod()
+        {
+            throw new BusinessException($"Неправильная логика {nameof(SecondMethod)}:");
+        }
+
+        public void ThirdMethod()
+        {
+            throw new Exception($"Сломал логику {nameof(ThirdMethod)}");
+        }
+    }
+}
---
+++ Modul2HW5/Modul2HW5/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace Modul2HW5
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var starter = new AppStarter();
+            starter.Run();
+            Console.ReadKey();
+        }
+    }
+}
